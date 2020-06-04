19c23cc5668640cb5764e20ce550bcd3d2d8d3a0
Adel Rashed <adel_rashed@outlook.com>
2020-06-04 20:58:35 +0000

    implemented asynch findMyIp function to find public ip address

---
+++ iss.js
@@ -0,0 +1,22 @@
+const request = require("request");
+
+const fetchMyIP = function (callback) {
+  const url = "https://api.ipify.org/?format=json";
+  request(url,(error,response,body) => {
+    if (error) {
+      callback(error,null);
+    };
+    
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
+
+    callback(null,JSON.parse(body).ip);
+
+
+    
+  });
+};
+
+module.exports = {fetchMyIP};
