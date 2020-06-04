471ca7f726d145ac5714ffa0cea61730d7974344
Adel Rashed <adel_rashed@outlook.com>
2020-06-04 23:47:33 +0000

    nextISSTimesForMyLocation and it should take in just a callback for handling the resulting data (or error).

--- iss.js
+++ iss.js
@@ -25,6 +25,7 @@ const fetchCoordByIP = function(ip,callback) {
   request(url,(error,response,body) => {
     if (error) {
       callback(error,null);
+      return;
     }
     
     if (response.statusCode !== 200) {
@@ -39,4 +40,58 @@ const fetchCoordByIP = function(ip,callback) {
   });
 };
 
-module.exports = {fetchMyIP,fetchCoordByIP};
+const fetchISSFlyOverTimes = function(coords,callback) {
+  const {latitude,longitude} = coords;
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
+  request(url,(error,response,body) => {
+    if (error) {
+      callback(error,null);
+      return;
+    }
+    
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching coordinates for  IP: ${body}`), null);
+      return;
+    }
+    const flyOverTimes = JSON.parse(body).response;
+    callback(null,flyOverTimes);
+
+
+    
+  });
+
+};
+
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      callback(error,null);
+      return;
+    }
+
+    fetchCoordByIP(ip,(error, coords) => {
+      if (error) {
+        callback(error,null);
+        return;
+      }
+      
+      fetchISSFlyOverTimes(coords,(error, flyTimes) => {
+        if (error) {
+          callback(error,null);
+          return;
+        }
+      
+        callback(null,flyTimes);
+      });
+
+
+
+    });
+  });
+
+
+  
+};
+
+//module.exports = {fetchMyIP,fetchCoordByIP,fetchISSFlyOverTimes};
+module.exports = {nextISSTimesForMyLocation};
