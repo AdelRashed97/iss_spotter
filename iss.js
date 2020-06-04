2033aa483722a4ed5a7b1a8b39bfc6a583815e44
Adel Rashed <adel_rashed@outlook.com>
2020-06-04 20:59:40 +0000

    fixed lint issues

--- iss.js
+++ iss.js
@@ -1,11 +1,11 @@
 const request = require("request");
 
-const fetchMyIP = function (callback) {
+const fetchMyIP = function(callback) {
   const url = "https://api.ipify.org/?format=json";
   request(url,(error,response,body) => {
     if (error) {
       callback(error,null);
-    };
+    }
     
     if (response.statusCode !== 200) {
       callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
