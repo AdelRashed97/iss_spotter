e56f5c1e357cca70b19c347157123d88cf60c4dc
Adel Rashed <adel_rashed@outlook.com>
2020-06-05 04:21:48 -0400

    refactored fetchISSFlyOverTimes and nextISSTimesForMyLocation using promises

--- iss_promised.js
+++ iss_promised.js
@@ -9,6 +9,27 @@ const fetchCoordByIP = function(body) {
   const ip = JSON.parse(body).ip;
   const url = `https://ipvigilante.com/json/${ip}`;
   return request(url);
+};
+
+
+
+const fetchISSFlyOverTimes = function(body) {
+  const {latitude,longitude} = JSON.parse(body).data;
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+
+};
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMYIP()
+    .then(fetchCoordByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+  
+
 }
 
-module.exports = {fetchMYIP,fetchCoordByIP};
+module.exports = {nextISSTimesForMyLocation};
